c7a2a06a5ab26af06b0edbb66c183bce10ee79a9
aluminiumgeek <mihail265@gmail.com>
2014-04-20 15:22:05 +0400

    handlers to put and get values from the store

---
+++ src/data.erl
@@ -0,0 +1,42 @@
+-module(data).
+
+-behavior(e2_service).
+
+-export([start_link/1, get/1, get/2, put/2, del/1]).
+
+-export([init/1, handle_msg/3]).
+
+start_link(File) ->
+    e2_service:start_link(?MODULE, File, [registered]).
+
+get(Key) ->
+    e2_service:call(?MODULE, {get, Key}).
+
+get(Key, Default) ->
+    e2_service:call(?MODULE, {get, Key, Default}).
+
+put(Key, Value) ->
+    e2_service:call(?MODULE, {put, Key, Value}).
+
+del(Key) ->
+    e2_service:call(?MODULE, {del, Key}).
+
+
+init(File) ->
+    {ok, open_db(File)}.
+
+open_db(File) ->
+    {ok, Db} = store:open(File), Db.
+
+
+handle_msg({get, Key}, _From, Db) ->
+    {reply, store:get(Db, Key), Db};
+
+handle_msg({get, Key, Default}, _From, Db) ->
+    {reply, store:get(Db, Key, Default), Db};
+
+handle_msg({put, Key, Value}, _From, Db) ->
+    {reply, store:put(Db, Key, Value), Db};
+
+handle_msg({del, Key}, _From, Db) ->
+    {reply, store:del(Db, Key), Db}.
--- src/server_rest_handler.erl
+++ src/server_rest_handler.erl
@@ -1,6 +1,6 @@
 -module(server_rest_handler).
 
--export([handle_http/1, handle/3]).
+-export([handle_http/1]).
 
 handle_http(Req) ->
     handle(
@@ -10,4 +10,37 @@ handle_http(Req) ->
     ).
 
 handle('GET', [], Req) ->
-    Req:ok([{"Content-Type", "text/plain"}], "Test.").
+    handle_reply({ok, "API is running"}, Req);
+
+handle('GET', ["store", Key], Req) ->
+    handle_reply(store_get(Key), Req);
+
+handle('POST', ["store", Key], Req) ->
+    case lists:filter(fun({Param, _}) -> string:equal(Param, "value") end, Req:parse_post()) of
+        [{_, Value}] ->
+            handle_reply(store_put(Key, Value), Req);
+        [] ->
+            handle_reply(error, Req);
+    end.
+    
+
+
+store_get(Key) ->
+    data:get(Key).
+
+store_get(Key, Default) ->
+    data:get(Key, Default).
+
+store_put(Key, Value) ->
+    data:put(Key, Value).
+
+store_del(Key) ->
+    data:del(Key).
+
+
+handle_reply({ok, Value}, Req) ->
+    Req:ok([{"Content-Type", "text/plain"}], Value);
+handle_reply(ok, Req) ->
+    Req:ok([{"Content-Type", "text/plain"}], "ok");
+handle_reply(error, Req) ->
+    Req:ok([{"Content-Type", "text/plain"}], "error").
