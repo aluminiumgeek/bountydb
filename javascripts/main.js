bc38ebcb0b421587dc17a271b6abd30e2004ac5f
aluminiumgeek <mihail265@gmail.com>
2014-04-26 19:09:49 +0400

    update website

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log('This would be the main JS file.');
+(function($) {
+  var hostname = "http://psychedelizer.0x80.ru:3334"
+  $(document).ready(function() {
+    $('.save-key form').submit(function(e) {
+      e.preventDefault();
+      
+      var key = $('input[name="key"]').val();
+      var value = $('input[name="value"]').val();
+      
+      var data = {
+        value: value
+      }
+      
+      if (key && value) {
+        $.ajax({
+          url: hostname + '/store/' + key,
+          method: 'put',
+          data: JSON.stringify(data)
+        }).done(function(data) {
+          console.log(data);
+        });
+      }
+    });
+  });
+})(jQuery);
